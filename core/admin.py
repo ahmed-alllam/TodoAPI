dab7cbbd9e3281ecfbf92cc32e92a80b10883590
Ahmed Emad <ahmed39652003@gmail.com>
2020-02-29 19:29:09 +0200

    registered models to admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+#   Copyright (c) Code Written and Tested by Ahmed Emad in 29/02/2020, 19:29
+#
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.models import Group
+
+from core.models import UserProfileModel, TodoGroupModel, TodoModel, TodoAttachmentModel
+
+admin.site.unregister(Group)
+admin.site.register(UserProfileModel)
+admin.site.register(TodoGroupModel)
+admin.site.register(TodoModel)
+admin.site.register(TodoAttachmentModel)
